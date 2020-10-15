ee31c190fd0f80d3ed6971c40d4b6a46a335a6f2
Zara Cooper <zramcooper@gmail.com>
2020-10-15 21:56:33 +0300

    Add UntilDestroy decorator

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { NetlifyFormsService } from '../netlify-forms/netlify-forms.service';
 
+@UntilDestroy()
 @Component({
   selector: 'app-feedback',
   templateUrl: './feedback.component.html',
   styleUrls: ['./feedback.component.css']
 })
-export class FeedbackComponent {
+export class FeedbackComponent{
   feedbackForm = this.fb.group({
     firstName: ['', Validators.required],
     lastName: ['', Validators.required],
@@ -27,7 +29,9 @@ export class FeedbackComponent {
   ) { }
 
   onSubmit() {
-    this.netlifyForms.submitFeedback(this.feedbackForm.value)
+    this.netlifyForms
+      .submitFeedback(this.feedbackForm.value)
+      .pipe(untilDestroyed(this))
       .subscribe(
         () => this.feedbackForm.reset(),
         err => this.errorMsg = err,
